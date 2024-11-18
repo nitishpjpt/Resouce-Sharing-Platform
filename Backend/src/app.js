8e9d85c41b43878748e11ed562da5ea3d60caac1
nitishpjpt <nitishpjpt97@gmail.com>
2024-11-18 10:26:25 +0530

    changes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,13 +5,11 @@ import userRouter from "./router/user.routes.js";
 
 const app = express();
 
-//cors
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: 'https://resouce-sharing-platform-k9nx.vercel.app', // Replace with your frontend domain
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow necessary HTTP methods
+  credentials: true // If using cookies or authentication headers
+}));
 // express
 app.use(express.json({ limit: "16kb" }));
 app.use(express.static("public"));
