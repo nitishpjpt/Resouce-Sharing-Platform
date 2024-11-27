fc3a879dade9e2867b3855ce5e2900b07edf9f81
nitishpjpt <nitishpjpt97@gmail.com>
2024-11-27 15:09:16 +0530

    some changes

--- Frontend/src/components.part/Teacher.jsx
+++ Frontend/src/components.part/Teacher.jsx
@@ -31,7 +31,7 @@ const Teacher = () => {
     try {
       // Replace with your API endpoint
       const response = await axios.post(
-        "http://localhost:8000/api/v1/user/teachers",
+        "https://resouce-sharing-platform-backend.vercel.app/api/v1/user/teachers",
         formData,
         {
           headers: {
